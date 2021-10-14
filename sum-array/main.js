6e883b415f455b416e8deb10bd3fb857cb1d7df5
basiltime <krystalcanfield04@gmail.com>
2021-10-14 13:37:23 -0400

    Sum Mixed Array 8kyu

---
+++ sum-array/main.js
@@ -0,0 +1,27 @@
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+function sumMix(arr) {
+  return arr.reduce((acc, cv) => acc + Number(cv), 0);
+}
+
+function sumMix(x) {
+  return x.map((a) => +a).reduce((a, b) => a + b);
+}
+
+function sumMix(x) {
+  let result = 0;
+  for (let n of x) {
+    result += parseInt(n);
+  }
+  return result;
+}
+
+function sumMix(x){
+    var somme = 0;
+    for (i = 0; i<x.length; i++) {
+      somme += Number(x[i]);
+    }
+    return somme;
+  }
