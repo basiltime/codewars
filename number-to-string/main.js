2cb95d8d233f61457e05613352b65b4bbdc9ba33
basiltime <krystalcanfield04@gmail.com>
2021-10-16 13:23:30 -0400

    Convert a Number to a String

--- number-to-string/main.js
+++ number-to-string/main.js
@@ -1,3 +1,19 @@
 function numberToString(num) {
     return(num.toString())
   }
+
+
+  function numberToString(num) {
+    return String(num);
+  }
+
+
+  const numberToString = num => `${num}`;
+
+  
+  function numberToString(num) {
+    return `${num}`;
+  }
+
+
+
