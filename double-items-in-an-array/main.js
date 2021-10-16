338e8fe7008fecb6d930c2f69cd5ce263de06b83
basiltime <krystalcanfield04@gmail.com>
2021-10-16 17:15:41 -0400

    Beginner- Lost Without a Map 8kyu

--- double-items-in-an-array/main.js
+++ double-items-in-an-array/main.js
@@ -1,7 +1,11 @@
-function maps(x){
-    let newArray = []
-    for (number of x) {
-      newArray.push(number * 2)
-    }
-    return newArray
+function maps(x) {
+  let newArray = [];
+  for (number of x) {
+    newArray.push(number * 2);
   }
+  return newArray;
+}
+
+function maps(x) {
+  return x.map((x) => x * 2);
+}
