ca7530b14607dd50b9daa008169d5160dc7c23e7
basiltime <krystalcanfield04@gmail.com>
2021-10-16 22:14:00 -0400

    Reversed Strings 8kyu

---
+++ my-head-is-at-the-wrong-end/main.js
@@ -0,0 +1,13 @@
+function fixTheMeerkat(arr) {
+    let fixedMeerkat = []
+    for (i of arr) {
+      fixedMeerkat.unshift(i)
+    }
+    return fixedMeerkat
+  }
+
+  function fixTheMeerkat(arr) {
+    return arr.reverse();
+  }
+
+  
