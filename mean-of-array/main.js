b94d21308a6eab4c874b0610c55c5d9647c2d28a
basiltime <krystalcanfield04@gmail.com>
2021-10-16 23:54:38 -0400

    Get The Mean Of An Array 8kyu

---
+++ mean-of-array/main.js
@@ -0,0 +1,8 @@
+function getAverage(marks) {
+  const reducer = (previousValue, currentValue) => previousValue + currentValue;
+  return Math.floor(marks.reduce(reducer) / marks.length);
+}
+
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+// Return the average of the given array rounded down to its nearest integer.
+// The array will never be empty.
